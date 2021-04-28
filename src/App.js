f755499cc5d1348acec48e4ba4ad35793bce18b8
Teph Gab <tephgab@gmail.com>
2021-04-28 16:47:16 -0400

    Show tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Meeting at Scool',
+        day: 'Feb 6th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Shopping',
+        day: 'Feb 6th at 2:30pm',
+        reminder: false
+    }
+]);
+  
   return (
     <div className="container">
      <Header title="Task tracker"/>
+     <Tasks tasks={tasks} />
     </div>
   );
 }
